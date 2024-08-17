9b8749e7bef830851127e0f087af9fb5de4e512d
tada-js <justfix010@gmail.com>
2024-08-18 02:56:19 +0900

    Add : 프로그래머스 레벨2 두개뽑아서더하기

--- programmers/level_1/두개뽑아서더하기.js
+++ programmers/level_1/두개뽑아서더하기.js
@@ -1,12 +1,32 @@
-function solution(numbers) {
-  const set = new Set();
+{
+  function solution(numbers) {
+    const set = new Set();
 
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < i; j++) {
-      set.add(numbers[i] + numbers[j]);
+    for (let i = 0; i < numbers.length; i++) {
+      for (let j = 0; j < i; j++) {
+        set.add(numbers[i] + numbers[j]);
+      }
     }
-  }
 
-  return [...set].sort((a, b) => a - b);
+    return [...set].sort((a, b) => a - b);
+  }
+  solution([5, 0, 2, 7]);
 }
-solution([5, 0, 2, 7]);
+{
+  // 스택 사용
+  function solution(numbers) {
+    const stack = [...numbers];
+    const result = [];
+
+    while (stack.length > 1) {
+      const num = stack.pop();
+      const temp = [...stack];
+
+      for (let i = 0; i < temp.length; i++) {
+        result.push(num + temp[i]);
+      }
+    }
+
+    return [...new Set(result)].sort((a, b) => a - b);
+  }
+}
